f787d285ec393a238221126eac6ab9014ff1d611
Guillermo <guigommor@alu.edu.gva.es>
2023-12-14 17:21:36 +0100

    metodo  leerNotas(totalAlumnos)

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class Evaluacion {
 
     String nombreAsignatura;
-    double listaNotas[];
+    double[] listaNotas;
 
 
     public Evaluacion(String nombreAsignatura) {
@@ -12,11 +14,32 @@ public class Evaluacion {
         this.nombreAsignatura = nombreAsignatura;
         this.listaNotas = listaNotas;
     }
-    public void leerNotas(int totalAlumnos){
-        if (listaNotas==null) {
-            System.out.println("Notas ya introducidas desea introducirlas de nuevo(s/n)?");
 
-        }
+    public void leerNotas(int totalAlumnos) {
+        Scanner sc = new Scanner(System.in);
+        String eleccion;
+        if (listaNotas != null) {
+            System.out.println("Notas ya introducidas desea introducirlas de nuevo(s/n)?");
+            eleccion = sc.nextLine();
+
+            if (eleccion.equalsIgnoreCase("s")) {
+                this.listaNotas = new double[totalAlumnos];
+                double nota;
+
+                for (int j = 0; j < totalAlumnos; j++) {
+                    do {
+                        System.out.println("Introduzca la nota entre 0 y 10 (Ej 8,5)");
+                        nota = sc.nextDouble();
+                    } while (0 < nota && nota > 10);
+                    listaNotas[j] = nota;
+                }
+
+            } else if (eleccion.equalsIgnoreCase("n")) {
+                for (int k = 0; k < totalAlumnos; k++) {
+                    System.out.println(listaNotas[k]);
+                }
+            }
         }
     }
+}
 
