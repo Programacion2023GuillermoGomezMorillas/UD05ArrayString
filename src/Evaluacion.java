81c2b703d9f863249cdd3ecda3818f6d3609be3b
Guillermo1KDAM <guigommor@alu.edu.gva.es>
2023-12-14 19:56:36 +0100

    Pruebas

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -4,6 +4,7 @@ public class Evaluacion {
 
     String nombreAsignatura;
     double[] listaNotas;
+    Scanner sc;
 
 
     public Evaluacion(String nombreAsignatura) {
@@ -16,29 +17,36 @@ public class Evaluacion {
     }
 
     public void leerNotas(int totalAlumnos) {
-        Scanner sc = new Scanner(System.in);
         String eleccion;
         if (listaNotas != null) {
             System.out.println("Notas ya introducidas desea introducirlas de nuevo(s/n)?");
             eleccion = sc.nextLine();
 
             if (eleccion.equalsIgnoreCase("s")) {
-                this.listaNotas = new double[totalAlumnos];
-                double nota;
-
-                for (int j = 0; j < totalAlumnos; j++) {
-                    do {
-                        System.out.println("Introduzca la nota entre 0 y 10 (Ej 8,5)");
-                        nota = sc.nextDouble();
-                    } while (0 < nota && nota > 10);
-                    listaNotas[j] = nota;
-                }
+                crearLista(totalAlumnos);
 
             } else if (eleccion.equalsIgnoreCase("n")) {
                 for (int k = 0; k < totalAlumnos; k++) {
                     System.out.println(listaNotas[k]);
                 }
             }
+        }
+    }
+
+    /**
+     * Método para introducir las notas en el array
+     */
+    public void crearLista(int totalAlumnos) {
+        this.listaNotas = new double[totalAlumnos];
+        Scanner sc = new Scanner(System.in);
+        double nota;
+        for (int j = 0; j < totalAlumnos; j++) {
+            do {
+                System.out.println("Introduzca la nota entre 0 y 10 (Ej 8,5)");
+                nota = sc.nextDouble();
+            } while (0 < nota && nota > 10);
+
+
         }
     }
 }
