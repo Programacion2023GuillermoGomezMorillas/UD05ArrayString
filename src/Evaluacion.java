3e3552b24d1f59b90aacdadc739ff9b0cdcd5f54
Guillermo1KDAM <guigommor@alu.edu.gva.es>
2023-12-14 20:49:02 +0100

    Continuacion

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -4,7 +4,7 @@ public class Evaluacion {
 
     String nombreAsignatura;
     double[] listaNotas;
-    Scanner sc;
+    Scanner sc = new Scanner(System.in);
 
 
     public Evaluacion(String nombreAsignatura) {
@@ -31,23 +31,33 @@ public class Evaluacion {
                 }
             }
         }
+        else {
+            crearLista(totalAlumnos);
+        }
     }
-
     /**
      * Método para introducir las notas en el array
      */
     public void crearLista(int totalAlumnos) {
         this.listaNotas = new double[totalAlumnos];
-        Scanner sc = new Scanner(System.in);
         double nota;
         for (int j = 0; j < totalAlumnos; j++) {
             do {
                 System.out.println("Introduzca la nota entre 0 y 10 (Ej 8,5)");
                 nota = sc.nextDouble();
             } while (0 < nota && nota > 10);
+            System.out.print(listaNotas[j]);
+        }
 
+    }
 
+    @Override
+    public String toString() {
+        System.out.println("Asignatura: " + nombreAsignatura);
+        for (int j=0; j< listaNotas.length;j++) {
+            return "\nAlumno 1: " + listaNotas[0] + "\nAlumno 2: " + listaNotas[0] +
         }
     }
 }
+
 
