ed34603eaa9212cf37c700fe592fb127d5df3878
Guillermo1KDAM <guigommor@alu.edu.gva.es>
2023-12-19 20:22:26 +0100

    Fin Evaluacion2

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -1,36 +1,53 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * Se trata de una clase que contiene las notas de un grupo y puden ser consultadas, visualizadas y modificadas
+ */
 public class Evaluacion {
-
+    //Declaracion de variables
     private String nombreAsignatura;
     private double[] listaNotas;
     Scanner sc = new Scanner(System.in);
 
-
+    /**
+     * Constructor para el nombre de la asignatura
+     * @param nombreAsignatura
+     */
     public Evaluacion(String nombreAsignatura) {
         this.nombreAsignatura = nombreAsignatura;
     }
 
+    /**
+     * Constructor para el nombre de la asignatura y la lista de las notas
+     * @param nombreAsignatura
+     * @param listaNotas
+     */
     public Evaluacion(String nombreAsignatura, double[] listaNotas) {
         this.nombreAsignatura = nombreAsignatura;
         this.listaNotas = listaNotas;
     }
 
+    /**
+     *
+     * @param totalAlumnos Muestra las notas que hay, y si no hay, las crea para que el usuario las introduzca
+     */
     public void leerNotas(int totalAlumnos) {
         String eleccion;
+        //Si existe la lista, pregunta si quieres sobrescribir las notas o no
         if (listaNotas != null) {
             System.out.println("Notas ya introducidas desea introducirlas de nuevo(s/n)?");
             eleccion = sc.nextLine();
-
+            //Si la respuesta es que si, te envia a un metodo para crear la lista
             if (eleccion.equalsIgnoreCase("s")) {
                 crearLista(totalAlumnos);
-
+                //Si la respuesta es que no, te muestra la lista que habia antes
             } else if (eleccion.equalsIgnoreCase("n")) {
                 for (int k = 0; k < totalAlumnos; k++) {
                     System.out.print(listaNotas[k] + ", ");
                 }
             }
+            //Si no existe lista, crea una nueva
         } else {
             crearLista(totalAlumnos);
         }
@@ -42,6 +59,7 @@ public class Evaluacion {
     public void crearLista(int totalAlumnos) {
         this.listaNotas = new double[totalAlumnos];
         double nota;
+        //Bucle para introducir las notas dependiendo de los alumnos totales
         for (int j = 0; j < totalAlumnos; j++) {
             do {
                 System.out.println("Introduzca la nota entre 0 y 10 (Ej 8,5)");
@@ -70,6 +88,11 @@ public class Evaluacion {
         return listaAlumnos;
     }
 
+    /**
+     * Se introduce una lista y se comprueba si existe o no existe la lista, si no existe devuelve -1
+     * @param compruebaLista
+     * @return
+     */
     public int comprobarNotas(double[] compruebaLista) {
         if (compruebaLista == null) {
             return -1;
@@ -78,12 +101,18 @@ public class Evaluacion {
         }
     }
 
+    /**
+     * Devuelve la media de las notas de calse
+     * @return
+     */
     public double media() {
         int comprobante = comprobarNotas(listaNotas);
         double media = 0;
+        //Comprobante por si no existe la lista
         if (comprobante == -1) {
             System.out.println("Sin notas por el momento");
         } else {
+            //Bucle para sumar todos los valores
             for (int i = 0; i < listaNotas.length; i++) {
                 media += listaNotas[i];
             }
@@ -91,12 +120,18 @@ public class Evaluacion {
         return (media/ listaNotas.length);
     }
 
+    /**
+     * Metodo para devolver la mínima nota de la lista
+     * @return
+     */
     public double minimo() {
         int comprobante = comprobarNotas(listaNotas);
         double minimo = 10;
+        //Comprobante por si no existe la lista
         if (comprobante == -1) {
             System.out.println("Sin notas por el momento");
         } else {
+            //Bucle para sumar todos los valores
             for (int i = 0; i < listaNotas.length; i++){
                 if (listaNotas[i] < minimo){
                     minimo = listaNotas[i];
@@ -105,12 +140,19 @@ public class Evaluacion {
         }
         return minimo;
     }
+
+    /**
+     * Metodo para devolver la máxima nota de la lista
+     * @return
+     */
     public double maximo() {
         int comprobante = comprobarNotas(listaNotas);
         double maximo = 0;
+        //Comprobante por si no existe la lista
         if (comprobante == -1) {
             System.out.println("Sin notas por el momento");
         } else {
+            //Recorre el array para ver cual es el máximo
             for (int i = 0; i < listaNotas.length; i++){
                 if (listaNotas[i] > maximo){
                     maximo = listaNotas[i];
@@ -119,12 +161,19 @@ public class Evaluacion {
         }
         return maximo;
     }
+
+    /**
+     * Metodo para devolver el número de suspensos totales
+     * @return
+     */
     public int totalSuspensos() {
         int comprobante = comprobarNotas(listaNotas);
         int cont = 0;
+        //Comprobante por si no existe la lista
         if (comprobante == -1) {
             System.out.println("Sin notas por el momento");
         } else {
+            //Recorre el array para ver cual es el numero total de suspensos
             for (int i = 0; i < listaNotas.length; i++){
                 if (listaNotas[i] < 5){
                     cont++;
@@ -133,12 +182,19 @@ public class Evaluacion {
         }
        return cont;
     }
+
+    /**
+     * Método que devuelve el número de aprobados totales
+     * @return
+     */
     public int totalAprobados() {
         int comprobante = comprobarNotas(listaNotas);
         int cont = 0;
+        //Comprobante por si no existe la lista
         if (comprobante == -1) {
             System.out.println("Sin notas por el momento");
         } else {
+            //Recorre el array para ver cual es el numero total de aprobados
             for (int i = 0; i < listaNotas.length; i++){
                 if (listaNotas[i] > 4){
                     cont++;
@@ -147,25 +203,46 @@ public class Evaluacion {
         }
         return cont;
     }
+
+    /**
+     * Metodo para cambiar la nota de un alumno en concreto.
+     * Hay que meter la nota y el alumno al método
+     * @param nota
+     * @param alumno
+     */
     public void cambiarNota(double nota, int alumno){
         alumno = alumno-1;
+        //Si supera el tamaño devulve -1
         if (alumno > listaNotas.length || alumno < 0){
             System.out.println("No existe ese alumno");
         }
+        //Asigna la nota al alumno elegido
         else {
             listaNotas[alumno] = nota;
         }
     }
+
+    /**
+     * Método que devuelve la mejor nota de la clase
+     * @return
+     */
     public double mejorAlumno(){
         double mejor=0;
+        //Recorre el array y guarda el alumno con la nota mas alta
         for (int i=0; i<listaNotas.length; i++)
             if (listaNotas[i] == maximo()){
                 mejor = i;
             }
         return mejor;
     }
+
+    /**
+     * Método que devuelve la peor nota de la clase
+     * @return
+     */
     public double peorAlumno(){
         double peor=0;
+        //Recorre el array y guarda el alumno con la nota mas baja
         for (int i=0; i<listaNotas.length; i++)
             if (listaNotas[i] == minimo()){
                 peor = i;
@@ -173,20 +250,34 @@ public class Evaluacion {
         return peor;
     }
 
+    /**
+     * Devuelve la nota de un alumno en concreto
+     * @param indiceAlumno
+     * @return
+     */
     public double notaAlumno(int indiceAlumno){
         indiceAlumno = indiceAlumno-1;
+        //Si esta por encima o por debajo del tamaño del array devuelve -1
         if (indiceAlumno > listaNotas.length || indiceAlumno < 0){
             return -1;
         }
         return listaNotas[indiceAlumno];
     }
+
+    /**
+     * Te devuelve un array con con todos los aprobados
+     * @return
+     */
     public int[] dameAprobados(){
         int aprobados = totalAprobados();
         int[] alumnosAprobados = new int[aprobados];
+        //Si no hay aprobados
     if (aprobados == 0){
         System.out.println("No hay aprobados");
+        //Si si que hay aprobados
     }else {
         int j = 0;
+        //Recorre el array para ver cuales son los que estan aprobados
         for (int i=0 ; i<listaNotas.length ; i++){
             if (listaNotas[i]>4){
                 alumnosAprobados[j] = i+1;
@@ -198,13 +289,20 @@ public class Evaluacion {
     return alumnosAprobados;
     }
 
+    /**
+     * Te devuelve un array con con todos los suspendidos
+     * @return
+     */
     public int[] dameSuspensos(){
         int suspensos = totalSuspensos();
         int[] alumnosSuspensos = new int[suspensos];
+        //Si no hay aprobados
         if (suspensos == 0){
             System.out.println("No hay aprobados");
+            //Si si que hay aprobados
         }else {
             int j = 0;
+            //Recorre el array para ver cuales son los que estan suspensos
             for (int i=0 ; i<listaNotas.length ; i++){
                 if (listaNotas[i]<5){
                     alumnosSuspensos[j] = i + 1;
@@ -215,17 +313,29 @@ public class Evaluacion {
         System.out.println(Arrays.toString(alumnosSuspensos));
         return alumnosSuspensos;
     }
+
+    /**
+     * Ordena el array de menor a mayor
+     * @return
+     */
     public double[] ordenar(){
         double[] listaOrdenada;
+        //La siguiente genera una copia del array
         listaOrdenada = Arrays.copyOf(listaNotas, listaNotas.length);
+        //La siguiente linea ordena el array
         Arrays.sort(listaOrdenada);
         return listaOrdenada;
     }
+
+    /**
+     * Dependiendo de las notas te da un mensaje por pantalla
+     */
     public void analizaGrupo(){
         double[] listaAnalizar = ordenar();
         double dosTercios = 2/3 * listaNotas.length;
+        //Variables para saber la cantidad de notas en los rangos que queremos
         int cont1=0, cont2=0, cont3=0, cont4=0;
-        int probabilidad = (int) (listaAnalizar.length * 0.66);
+        //Bucle para ir recorriendo el array y ver en que rango estan los alumnos con sus notas
         for (int i =0 ; i<listaAnalizar.length ; i++){
             if (listaNotas[i]>7) {
                 cont1++;
@@ -238,6 +348,7 @@ public class Evaluacion {
             }
             cont4++;
         }
+        //Condicionales para saber el rango de las notas de la clase
         if (cont1>dosTercios){
             System.out.println("VAMOS FENOMENTAL");
         }
--- src/Principal.java
+++ src/Principal.java
@@ -30,68 +30,111 @@ public class Principal {
         }
     }
 
+    /**
+     * Menu de navegacion
+     * @return
+     */
     public static int menu() {
         Scanner sc = new Scanner(System.in);
         int opc;
         do {
-            System.out.println("1- Leer Notas\n2-Mostrar Notas\n3-Mostrar la Median\n4-Mostrar Maximo y Minimo\n5-Mostrar suspensos y aprobados");
+            System.out.println("1-Leer Notas\n2-Mostrar Notas\n3-Mostrar la Median\n4-Mostrar Maximo y Minimo\n5-Mostrar suspensos y aprobados\n6-Saber mejor y peor alumno\n7-Cambiar una nota de un alumno\n8-Todos los aprobados y suspendidos\n9-Ordena la lista de menor a mayor\n10-Analiza al grupo");
             opc = sc.nextInt();
         } while (opc < 1 || opc > 10);
         return opc;
     }
 
+    /**
+     * Comprueba las notas y te las muestra
+     */
     public void comprobarNotas() {
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{3, 2, 4, 6, 8});
         evaluacion.leerNotas(5);
         evaluacion.toString();
     }
 
+    /**
+     * Muentra las notas
+     */
     public void mostrarNotas() {
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{5, 4, 3, 2, 1});
         evaluacion.toString();
     }
 
+    /**
+     * Hace la media de los alumnos
+     */
     public void mostrarMedia() {
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{10, 8, 7, 6, 5});
         System.out.println(evaluacion.media());
     }
+
+    /**
+     * Te muestra la nota máxima y mínima de los alumnos
+     */
     public void mostrarMaximoMinimo(){
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{10, 8, 7, 6, 5});
         System.out.println(evaluacion.maximo());
         System.out.println(evaluacion.minimo());
 
     }
+
+    /**
+     * Muestra el total de aprobados y de suspensos
+     */
     public void suspensosAprobados(){
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{10, 3, 4, 6, 5});
         System.out.println("Hay " + evaluacion.totalAprobados() + " aprobados");
         System.out.println("Hay " + evaluacion.totalSuspensos() + " suspendidos");
     }
+
+    /**
+     * Muentra el mejor y el peor alumno
+     */
     public void mejorPeor(){
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{10, 3, 4, 6, 5});
         System.out.println(evaluacion.peorAlumno());
         System.out.println(evaluacion.mejorAlumno());
     }
+
+    /**
+     * Cambia la nota de un alumno
+     */
     public void cambiarNota(){
         Scanner sc = new Scanner(System.in);
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{10, 3, 4, 6, 5});
+        //Aqui pedimos el alumno que queremos cambiar
         System.out.println("¿Que alumno quieres cambiar?");
         int alumno = sc.nextInt();
         alumno = alumno-1;
         evaluacion.notaAlumno(alumno);
+        //Aqui pedimos la nota que queremos ponerle
         System.out.println("¿Que nota quieres ponerle?");
         int nota = sc.nextInt();
-        evaluacion.cambiarNota(8, alumno);
+        evaluacion.cambiarNota(nota, alumno);
     }
+
+    /**
+     * Devuelve el array con los aprobados y los suspendidos
+     */
     public void dameAprobadosSuspendidos(){
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{10, 3, 4, 6, 5});
         System.out.println(evaluacion.dameAprobados());
         System.out.println(evaluacion.dameSuspensos());
     }
+
+    /**
+     * Ordena la lista de menor a mayor
+     */
     public void ordernarLista(){
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{10, 3, 4, 6, 5});
         double[] listaOrdenada = evaluacion.ordenar();
         System.out.println(Arrays.toString(listaOrdenada));
     }
+
+    /**
+     * Metodo para analizar del grupo dependiendo de sus notas
+     */
     public void analizarGrupo(){
         Evaluacion evaluacion = new Evaluacion("Programación", new double[]{0, 3, 4, 6, 5});
         evaluacion.analizaGrupo();
