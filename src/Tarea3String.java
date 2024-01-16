215f3b5bc81b2b60d5a5b09842f270716a654d4c
Guillermo1KDAM <guigommor@alu.edu.gva.es>
2024-01-16 17:54:30 +0100

    masPruebas

--- src/Tarea3String.java
+++ src/Tarea3String.java
@@ -1,10 +1,17 @@
 public class Tarea3String {
 
-    public boolean esCorrectoNIF(String nif){
-        boolean nifCorrecto=false;
-        if(nif.length()==9 && nif.){
-            else if{
+    public boolean esCorrectoNIF(String nif) {
+        boolean nifCorrecto = false;
+        String[] letrasNif = {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+        int numeroNif = Integer.parseInt(nif.substring(0,7));
+        numeroNif % 23 =
+        if (nif.length() == 9 && nif.substring(0, 7).matches("[0-9]*") && nif.substring(8).matches("[A-Z]*") && numeroNif%23=) {
+            else if {
+            }
+
         }
+    }
+    public boolean comprobarDigitos(){
 
     }
 }
