69aeb30046dadd5c059c61649e56932c1710b9eb
Guillermo1KDAM <guigommor@alu.edu.gva.es>
2024-01-16 18:48:23 +0100

    masPruebasss

--- src/Principal3.java
+++ src/Principal3.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Principal3 {
+    public static void main(String[] args) {
+        Tarea3String dni = new Tarea3String();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime tu NIF");
+        String nif = sc.nextLine();
+        dni.esCorrectoNIF(nif);
+
+    }
 }
--- src/Tarea3String.java
+++ src/Tarea3String.java
@@ -2,16 +2,14 @@ public class Tarea3String {
 
     public boolean esCorrectoNIF(String nif) {
         boolean nifCorrecto = false;
-        String[] letrasNif = {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
-        int numeroNif = Integer.parseInt(nif.substring(0,7));
-        numeroNif % 23 =
-        if (nif.length() == 9 && nif.substring(0, 7).matches("[0-9]*") && nif.substring(8).matches("[A-Z]*") && numeroNif%23=) {
-            else if {
+        String letrasNif = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int soloNumeroNif = Integer.parseInt(nif.substring(0,7));
+        String letra = nif.substring(8);
+        int moduloNif = soloNumeroNif % 23;
+        if (nif.length() == 9 && nif.substring(0, 7).matches("[0-9]*") && nif.substring(8).matches("[A-Z]*") && nif.substring(8).equals(String.valueOf(letrasNif.charAt(moduloNif)))) {
+            nifCorrecto=true;
+            }
+        return nifCorrecto;
             }
 
-        }
-    }
-    public boolean comprobarDigitos(){
-
-    }
 }
